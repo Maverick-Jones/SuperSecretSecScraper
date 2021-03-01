17bba518f9b1a9279d4a1b4fe536b3dc79a4e391
Ming D <58600763+Maverick-Jones@users.noreply.github.com>
2021-02-28 23:40:19 -0500

    Weekend's work

--- Main.java
+++ Main.java
@@ -4,38 +4,49 @@ import java.io.IOException;
 //runner class
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        //print title
-        System.out.println("----------------------------");
-        System.out.println("The Super Secret SEC Scraper");
-        System.out.println("   Ming DeMers | Feb 2021");
-        System.out.println("----------------------------");
-        System.out.println("Begin Test...");
-				//URL that will be accessed
-        String myURL = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=D&company=&dateb=&owner=include&start=0&count=100&output=atom";
-        System.out.println("URL used: " + myURL);
-        // Open the file.
-        PrintWriter out = new PrintWriter("SECData.txt"); //TODO make it so it appends!
-        //write title and url
-        out.println("----------------------------");
-        out.println("The Super Secret SEC Scraper");
-        out.println("   Ming DeMers | Feb 2021");
-        out.println("----------------------------");
-        out.println("URL used: " + myURL + "\n");
+  public static void main(String[] args) throws IOException {
+    // print title
+    System.out.println("----------------------------");
+    System.out.println("The Super Secret SEC Scraper");
+    System.out.println("   Ming DeMers | Feb 2021");
+    System.out.println("----------------------------");
+    System.out.println("Begin Test...");
+    // URL that will be accessed
+    String myURL = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=D&company=&dateb=&owner=include&start=0&count=100&output=atom";
+    System.out.println("URL used: " + myURL);
+    // Open the file.
+    PrintWriter out = new PrintWriter("SECData.txt"); // TODO make it so it appends!
+    // write title and url
+    out.println("----------------------------");
+    out.println("The Super Secret SEC Scraper");
+    out.println("   Ming DeMers | Feb 2021");
+    out.println("----------------------------");
+    out.println("URL used: " + myURL + "\n");
 
-        //print RSS feed to txt file
-        out.println(ReadRSS.readRSSFeed(myURL));
-        out.close(); //TODO ensure it prints properly
-				System.out.println("File write done.");
 
-        //print out the data, too
-        System.out.println(ReadRSS.readRSSFeed(myURL));
+    Validity myCompany = new Validity(myURL);
 
-				Validity myCompany = new Validity("LLC");
-				//System.out.println(Validity.checkCompany());
+    myCompany.checkValidName();
+    if (myCompany.valid==false)
+      System.out.println("False");
+    else
+      System.out.println("True");
+    
 
-				
-        //todo add a "points system" to ensure which is most helpful, etc.
 
-    }
+      
+
+    // print RSS feed to txt file
+    out.println(ReadRSS.readRSSFeed(myURL));
+    out.close(); // TODO ensure it prints properly
+    System.out.println("File write done.");
+
+    // print out the data, too
+    System.out.println(ReadRSS.readRSSFeed(myURL));
+
+    // System.out.println(Validity.checkCompany());
+
+    // todo add a "points system" to ensure which is most helpful, etc.
+
+  }
 }
--- Validity.java
+++ Validity.java
@@ -1,16 +1,13 @@
 import java.util.ArrayList;
 
-public class Validity { 
-	//class variables
+public class Validity {
+	// class variables
 	String companyName;
 	public boolean valid = true;
-	int objPoints = 0;
-	String registration;
-	int amountSold = 0;
 	int points = 0;
 	int i;
 
-	//two lists to containing words of interest
+	// two lists to containing words of interest
 	ArrayList<String> buzzWord = new ArrayList<>();
 	ArrayList<String> badWord = new ArrayList<>();
 
@@ -23,37 +20,29 @@ public class Validity {
 		badWord.add("INC");
 	}
 
-	//object constructor
-	public Validity(String name) {
-		this.companyName = name;
+	// object constructor
+	public Validity(String str) {
+		this.companyName = str;
 	}
 
-	//envokes all following methods for points and valididity
-	public int checkCompany () {
-		checkValid();
-		checkBuzzwords();
-		
-		return points;
-	}
-
-	//check to see if the name involves badWords, which automatically DQ's the company
-	public void checkValid() {
-		for (i=0; i<badWord.size(); i++) {
+	// envokes all following methods for points and valididity
+	public void checkValidName() {
+		for (i = 0; i < badWord.size() && valid == true; i++) {
 			if (companyName.contains(badWord.get(i)))
 				valid = false;
 		}
 	}
 
-	//checking for buzzWord, adds a "point" if there is one
-	public void checkBuzzwords () {
-		for (i=0; i<buzzWord.size(); i++) {
+	// checking for buzzWord, adds a "point" if there is one
+	public void checkBuzzwords() {
+		for (i = 0; i < buzzWord.size(); i++) {
 			if (companyName.contains(buzzWord.get(i)))
 				points++;
 		}
 	}
 
-	//returns amount processed in the sale
-	public void findAmountSold (){
+	// returns amount processed in the sale
+	public void findAmountSold() {
 
 	}
 
